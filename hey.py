780d82eef33407ae50017a5d88afa8fdddbfc1d6
Chris Wallace <chris@bitcollider.io>
2019-07-24 12:33:36 +0100

    Add randomised greetings

--- hey.py
+++ hey.py
@@ -1,7 +1,18 @@
 from flask import Flask
+from random import choice
 
 app = Flask(__name__)
 
+names = [
+	'meatbags',
+	'jerks',
+	'punks',
+	'rascals',
+	'brigands',
+	'foolish mortals'	
+]
+
 @app.route('/')
 def hey():
-	return 'Hey meatbags!'
+	greeting = 'Hey {}!\n'.format(choice(names))
+	return greeting
